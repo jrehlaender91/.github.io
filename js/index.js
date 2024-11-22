243041c60189cb45ae181671fa22cb18bbb61ae1
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2024-11-22 08:07:31 -0600

    Final Project

--- js/index.js
+++ js/index.js
@@ -4,31 +4,34 @@ const cardPills = document.querySelectorAll('.card-body .badge-pill');
 const closePills = document.querySelectorAll('.badge-pill .close');
 const reset = document.querySelector('#reset .badge-pill');
 const cards = document.querySelectorAll('.card');
-const svg = document.querySelectorAll('.card-svg .bi');
+const toggler = document.querySelector('.navbar-toggler');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 60) {
-        nav.style.transition = "all 0.5s";
-        nav.style.backgroundColor = "rgb(142, 227, 255)";
-        nav.style.boxShadow = "0 3px 5px rgba(4, 4, 4, 0.15)";
-        
-    } else {
-        nav.style.backgroundColor = "transparent";
-        nav.style.boxShadow = "none";
+    if(window.innerWidth > 992) {
+        if (window.scrollY > 60) {
+            nav.style.transition = "all 0.5s";
+            nav.style.backgroundColor = "rgb(142, 227, 255)";
+            nav.style.boxShadow = "0 3px 5px rgba(4, 4, 4, 0.15)";
+            
+        } else {
+            nav.style.backgroundColor = "transparent";
+            nav.style.boxShadow = "none";
+        }
     }
-})
+});
 
 /* SVG Animations */
 cards.forEach(function (card) {
     card.addEventListener('mouseover', () => {
         card.children[0].children[1].classList.add('animate__animated', 'animate__bounce', 'animate__fast', 'animate__repeat-2');
-    })
-})
+    });
+});
+
 cards.forEach(function (card) {
     card.addEventListener('mouseleave', () => {
         card.children[0].children[1].classList.remove('animate__animated', 'animate__bounce', 'animate__fast', 'animate__repeat-2');
-    })
-})
+    });
+});
 
 
 /* Listen to the pills filter button */
@@ -39,9 +42,9 @@ pills.forEach(function (pill) {
                 cardPill.parentNode.parentNode.parentNode.parentNode.classList.remove('d-none');
                 e.target.parentNode.style.backgroundColor = ""; 
             } 
-        })
-    })
-})
+        });
+    });
+});
 
 closePills.forEach(function (close) {
     close.addEventListener('click', (e) => {
@@ -51,9 +54,9 @@ closePills.forEach(function (close) {
         cardPills.forEach(function (cardPill) {
             if(e.target.parentNode.previousElementSibling.previousElementSibling.innerHTML === cardPill.innerHTML)
                 cardPill.parentNode.parentNode.parentNode.parentNode.classList.add('d-none');
-        })
-    })
-})
+        });
+    });
+});
 
 reset.addEventListener('click', () => {
     cardPills.forEach(function (cardPill) {
@@ -61,7 +64,13 @@ reset.addEventListener('click', () => {
         
         closePills.forEach(function (close) {
                 close.parentNode.style.backgroundColor = "";
-                })
-        })
-    })
+                });
+        });
+});
+
+toggler.addEventListener('click', () => {
+    nav.style.transition = "all 0.5s";
+    nav.style.backgroundColor = "rgb(142, 227, 255)";
+    nav.style.boxShadow = "0 3px 5px rgba(4, 4, 4, 0.15)";
+});
 
